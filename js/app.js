bc8e5e614a09c52544936579bae7d480e687ee56
farkhadkasimov <farkhadkasimov92@gmail.com>
2022-10-03 12:52:19 +0500

    upd build

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function BackToApp(){
+    window.open("ronline://chat","_self");
+    console.log("back to chat");
+}
+
+function CreditCard(){
+    window.open("ronline://open_product_cards_debit","_self");
+    console.log("credit card");
+}
+
+function PrivetMir(){
+    window.open("https://privetmir.ru/","_self");
+    console.log("credit card");
+}
